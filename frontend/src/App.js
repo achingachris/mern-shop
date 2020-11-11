b8a086abd80ca027b52585e047820a789e82e534
ChrisAchinga <achinga.chris@gmail.com>
2020-11-12 00:12:57 +0300

    react front-end setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to Shop</h1>
+    </>
+  );
+}
+
+export default App;
