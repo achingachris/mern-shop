351078a95bfde24f1d0f59a9fe72236896f4d9ee
ChrisAchinga <achinga.chris@gmail.com>
2020-11-13 00:33:37 +0300

    mock api test

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const products = require('./data/products')
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API is runnning')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
 
 app.listen(5000, console.log('Server Running On 5000'))
