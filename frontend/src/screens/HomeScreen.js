e407f250929f7c185dc246ece0c80c5edecee7c4
ChrisAchinga <achinga.chris@gmail.com>
2020-11-12 21:50:22 +0300

    home screen

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import products from '../products'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
