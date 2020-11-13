7e78c267ed2f549f8ff4c016793e67666a269e77
ChrisAchinga <achinga.chris@gmail.com>
2020-11-13 13:49:39 +0300

    es6 modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const products = require('./data/products')
-const dotenv = require('dotenv')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
