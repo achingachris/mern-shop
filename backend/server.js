3f7a0240e97b7766df3a247a3bc35bcf0cc14ba7
ChrisAchinga <achinga.chris@gmail.com>
2020-11-15 10:53:43 +0300

    initialize mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
+import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
