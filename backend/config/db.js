08e31bd5fe609f3764d05de2ad7c3d8f935870d1
ChrisAchinga <achinga.chris@gmail.com>
2020-11-15 10:58:01 +0300

    colors console

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
 import connectDB from './config/db.js'
 
@@ -26,5 +27,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(`Server Running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold)
 )
