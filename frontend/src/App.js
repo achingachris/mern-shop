89206c2664c4b93fdd3930ec38d13fa0f4798ece
ChrisAchinga <achinga.chris@gmail.com>
2020-12-08 10:08:04 +0300

    header footer setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { Container } from 'react-bootstrap'
+// COmponents: custom
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>Copyright &copy; sryder</Col>
+            </Row>
+        </Container>
+    </footer>
+  }
+  
+  export default Footer
+  
