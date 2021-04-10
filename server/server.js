a8ff1bdd941ccc936d16b6ae031b7568a79677d1
ChrisAchinga <achinga.chris@gmail.com>
2021-04-10 14:39:34 +0300

    env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -16,4 +18,7 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server Running on port 5000'))
+const PORT = process.env.PORT || 5000
+const MODE = process.env.NODE_ENV
+
+app.listen(PORT, console.log(`Server Running in ${MODE} mode on port ${PORT}`))
