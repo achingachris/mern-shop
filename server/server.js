46edab629e7c9944300892111921ca04adb03c1f
ChrisAchinga <achinga.chris@gmail.com>
2021-04-10 13:24:27 +0300

    setup server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+  console.log(product)
+})
+
+app.listen(5000, console.log('running'))
