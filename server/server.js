0b15c7b6ed4dbc0c86389b586784402c571d86cc
ChrisAchinga <achinga.chris@gmail.com>
2021-04-10 15:23:01 +0300

    es modules config

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
 const app = express()
