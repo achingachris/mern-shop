6ca499bd7172ef4b6ff0338aec903ef2ca289ff9
ChrisAchinga <achinga.chris@gmail.com>
2021-04-17 15:11:25 +0300

    import connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
