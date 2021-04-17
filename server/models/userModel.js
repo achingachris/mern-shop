009340dfb020b249787cc4e29063f115152473ec
ChrisAchinga <achinga.chris@gmail.com>
2021-04-17 16:02:49 +0300

    created models

---
+++ server/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+// user data
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// create user instance
+const User = mongoose.model('User', userSchema)
+
+export default User
