d7c06621392a21dca1418f2d2d6834179d4a07b9
ChrisAchinga <achinga.chris@gmail.com>
2021-04-17 16:52:22 +0300

    data to seed

---
+++ server/data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@shopper.app',
+    password: 'xxxx',
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@mail.com',
+    password: 'xxxx',
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@mail.com',
+    password: 'xxxx',
+  },
+]
+
+export default users
