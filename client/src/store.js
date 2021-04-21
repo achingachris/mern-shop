790a4b9beb8e7dee0f7009da4278fbfd601d7b23
ChrisAchinga <achinga.chris@gmail.com>
2021-04-21 15:42:25 +0300

    add reducer store

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'rdux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 // a strore
 const reducer = combineReducers({})
 
 const initialState = {}
 
-const store = createStore(reducer, initialState)
+const middleware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export  default store
