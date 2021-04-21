77e7da6c99e827dea080257fdcac839a260bd348
ChrisAchinga <achinga.chris@gmail.com>
2021-04-21 13:00:39 +0300

    error middleware

--- server/server.js
+++ server/server.js
@@ -17,6 +17,23 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
+// custom 404
+app.use((req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+})
+
+// error handling
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(
