044bb450f0762de7c2db6959da99c07163af2d05
ChrisAchinga <achinga.chris@gmail.com>
2021-04-21 13:17:00 +0300

    error middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -18,21 +19,10 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 
 // custom 404
-app.use((req, res, next) => {
-  const error = new Error(`Not found - ${req.originalUrl}`)
-  res.status(404)
-  next(error)
-})
+app.use(notFound)
 
 // error handling
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-  })
-})
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
