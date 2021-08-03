1817566c8b30bca6c6c104fd1bcb60a7ff05ab4f
ChrisAchinga <achinga.chris@gmail.com>
2021-08-03 16:55:02 +0300

    seeder script

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,5 @@
 const products = [
   {
-    _id: '1',
     name: 'Airpods Wireless Bluetooth Headphones',
     image: '/images/airpods.jpg',
     description:
@@ -8,12 +7,11 @@ const products = [
     brand: 'Apple',
     category: 'Electronics',
     price: 89.99,
-    countInStock: 10,
+    countInStock: 3,
     rating: 4.5,
-    numReviews: 12,
+    numReviews: 4,
   },
   {
-    _id: '2',
     name: 'iPhone 11 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
@@ -21,12 +19,11 @@ const products = [
     brand: 'Apple',
     category: 'Electronics',
     price: 599.99,
-    countInStock: 7,
+    countInStock: 10,
     rating: 4.0,
-    numReviews: 8,
+    numReviews: 4,
   },
   {
-    _id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpg',
     description:
@@ -36,10 +33,9 @@ const products = [
     price: 929.99,
     countInStock: 5,
     rating: 3,
-    numReviews: 12,
+    numReviews: 3,
   },
   {
-    _id: '4',
     name: 'Sony Playstation 4 Pro White Version',
     image: '/images/playstation.jpg',
     description:
@@ -47,12 +43,11 @@ const products = [
     brand: 'Sony',
     category: 'Electronics',
     price: 399.99,
-    countInStock: 11,
+    countInStock: 10,
     rating: 5,
-    numReviews: 12,
+    numReviews: 3,
   },
   {
-    _id: '5',
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
@@ -62,10 +57,9 @@ const products = [
     price: 49.99,
     countInStock: 7,
     rating: 3.5,
-    numReviews: 10,
+    numReviews: 2,
   },
   {
-    _id: '6',
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
@@ -75,7 +69,7 @@ const products = [
     price: 29.99,
     countInStock: 0,
     rating: 4,
-    numReviews: 12,
+    numReviews: 4,
   },
 ]
 
