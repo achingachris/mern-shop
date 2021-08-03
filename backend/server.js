2213d01bef514982892a1c59092edf0150260ba3
ChrisAchinga <achinga.chris@gmail.com>
2021-08-03 13:49:06 +0300

    dotenv and modules

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
+
 const app = express()
-const products = require('./data/products')
 
 app.get('/', (req, res) => {
-  res.send('API Successfully running')
+  res.send('API is running....')
 })
 
 app.get('/api/products', (req, res) => {
-  res.send(products)
+  res.json(products)
 })
 
 app.get('/api/products/:id', (req, res) => {
@@ -15,4 +19,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
