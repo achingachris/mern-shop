08aa6e7a041e8e8b24182464cd27071b918f5488
ChrisAchinga <achinga.chris@gmail.com>
2021-08-04 14:40:18 +0300

    cart page: + and =

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -12,7 +12,7 @@ const Loader = () => {
         display: 'block',
       }}
     >
-      <span class='sr-only'>Loading...</span>
+      <span className='sr-only'>Loading...</span>
     </Spinner>
   )
 }
