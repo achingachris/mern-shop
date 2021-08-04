1d995c177c34253416ad7245c7e2858ff52f7a06
ChrisAchinga <achinga.chris@gmail.com>
2021-08-04 12:33:36 +0300

    error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +16,10 @@ app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
