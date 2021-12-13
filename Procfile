2a17432a4b9a8612c2a073713400e4267458663f
achingachris <achinga.chris@gmail.com>
2021-12-13 15:16:20 +0300

    /production trial

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node backend/server.js
+web: node server/app.js
--- server/app.js
+++ server/app.js
@@ -36,10 +36,10 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
