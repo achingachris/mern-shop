1ce495ab94a88b82a00e44c923a8abcfe2c2ffd4
Joshua759611 <musyokijoxhua@gmail.com>
2022-05-05 23:07:31 -0700

    Mongodb connected successfully

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose'
 
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    //const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn=await mongoose.connect(
+        `mongodb+srv://DevJoshua:Joshua@swahilipothub.f8p1u.mongodb.net/SwahilipotHub?
+        retryWrites=true&w=majority`,{
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
