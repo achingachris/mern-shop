1e9be797e7830712c8f43dbf65881f68071b7df3
chrisdevcode <achinga.chris@gmail.com>
2022-06-01 21:53:45 +0300

    fix margin-top

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import { Container } from 'react-bootstrap'
-import Header from './components/Header'
+// import Header from './components/Header'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import HomeScreen from './pages/HomeScreen'
@@ -20,43 +20,43 @@ import ProductListScreen from './pages/ProductListScreen'
 import ProductEditScreen from './pages/ProductEditScreen'
 import OrderListScreen from './pages/OrderListScreen'
 
+// PAGE LAYOUTS
+import { NavBar } from './layout/NavBar'
+
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Route render={({ history }) => <SearchBar history={history} />} />
-      <main className='py-3'>
-          <Route path='/order/:id' component={OrderScreen} />
-          <Route path='/shipping' component={ShippingScreen} />
-          <Route path='/payment' component={PaymentScreen} />
-          <Route path='/placeorder' component={PlaceOrderScreen} />
-          <Route path='/login' component={LoginScreen} />
-          <Route path='/register' component={RegisterScreen} />
-          <Route path='/profile' component={ProfileScreen} />
-          <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-          <Route
-            path='/admin/productlist'
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={ProductListScreen}
-            exact
-          />
-          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
-          <Route path='/admin/orderlist' component={OrderListScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} exact />
-          <Route path='/page/:pageNumber' component={HomeScreen} exact />
-          <Route
-            path='/search/:keyword/page/:pageNumber'
-            component={HomeScreen}
-            exact
-          />
-          <Route path='/' component={HomeScreen} exact />
+      {/* <Header /> */}
+      <NavBar />
+      <main className='py-3 mt-5'>
+        <Route render={({ history }) => <SearchBar history={history} />} />
+        <Route path='/order/:id' component={OrderScreen} />
+        <Route path='/shipping' component={ShippingScreen} />
+        <Route path='/payment' component={PaymentScreen} />
+        <Route path='/placeorder' component={PlaceOrderScreen} />
+        <Route path='/login' component={LoginScreen} />
+        <Route path='/register' component={RegisterScreen} />
+        <Route path='/profile' component={ProfileScreen} />
+        <Route path='/product/:id' component={ProductScreen} />
+        <Route path='/cart/:id?' component={CartScreen} />
+        <Route path='/admin/userlist' component={UserListScreen} />
+        <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+        <Route path='/admin/productlist' component={ProductListScreen} exact />
+        <Route
+          path='/admin/productlist/:pageNumber'
+          component={ProductListScreen}
+          exact
+        />
+        <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+        <Route path='/admin/orderlist' component={OrderListScreen} />
+        <Route path='/search/:keyword' component={HomeScreen} exact />
+        <Route path='/page/:pageNumber' component={HomeScreen} exact />
+        <Route
+          path='/search/:keyword/page/:pageNumber'
+          component={HomeScreen}
+          exact
+        />
+        <Route path='/' component={HomeScreen} exact />
       </main>
       <Footer />
     </Router>
