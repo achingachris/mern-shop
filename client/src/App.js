4d4e350f111394043c1d32613c5f93d3556da305
chrisdevcode <achinga.chris@gmail.com>
2022-06-02 00:52:48 +0300

    main search bar --change directory

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import { Container } from 'react-bootstrap'
 // import Header from './components/Header'
 import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
 import HomeScreen from './pages/HomeScreen'
 import ProductScreen from './pages/ProductScreen'
 import CartScreen from './pages/CartScreen'
@@ -22,6 +21,7 @@ import OrderListScreen from './pages/OrderListScreen'
 
 // PAGE LAYOUTS
 import { NavBar } from './layout/NavBar'
+import SearchBar from './layout/SearchBar'
 
 const App = () => {
   return (
